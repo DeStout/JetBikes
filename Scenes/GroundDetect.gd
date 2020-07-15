b49723ccb2646287c0efe0ca4ea6319ea13b1bf7
dstout <dstout@optimaxsi.com>
2020-07-15 14:59:06 -0400

    Added player movement and ground normal alignment.

---
+++ Scenes/GroundDetect.gd
@@ -0,0 +1,10 @@
+extends RayCast
+
+signal is_colliding
+
+func _ready():
+	connect("is_colliding", get_parent(), "_is_on_ground")
+	
+func _process(delta):
+	if is_colliding():
+		emit_signal("is_colliding")
