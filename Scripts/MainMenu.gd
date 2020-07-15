36621962c0f85343daff06854410e00b76ed02f9
De Stout <destoutdev@gmail.com>
2020-07-15 00:17:52 -0400

    Added camera\player rotations.

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 func on_ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+func _on_Solo_Button_pressed():
+	get_tree().change_scene("res://Scenes/TestTrack.tscn")
 
 func _on_Quit_Button_pressed():
 	get_tree().quit()
