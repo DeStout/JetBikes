923e261c24887a65be0b211f23d488207aa310d7
DeStout <destoutdev@gmail.com>
2022-08-27 23:03:57 -0700

    Continued improvements to OnlineMenu joypad input.

--- Menus/MainMenu/3DMenu/OnlineMenu.gd
+++ Menus/MainMenu/3DMenu/OnlineMenu.gd
@@ -20,79 +20,106 @@ func _hide_show() -> void:
 
 	if Input.get_connected_joypads().size():
 		if current_focus == null:
-			current_focus = $HostJoin/HostBox
+			current_focus = host_box
 		current_focus.grab_focus()
 		yield(get_tree(), "idle_frame")
 
 
 func _input(event: InputEvent) -> void:
 	if Input.get_connected_joypads().size():
-		if event.is_action_pressed("ui_up") or (event is InputEventJoypadMotion and \
+		var temp_focus : Control
+		if Input.is_action_just_pressed("ui_up") or (event is InputEventJoypadMotion and \
 								event.axis == JOY_AXIS_1 and event.axis_value == -1):
-				current_focus = current_focus.get_node(current_focus.focus_neighbour_top)
+				temp_focus = current_focus.get_node(current_focus.focus_neighbour_top)
+				if !temp_focus.focus_mode == Control.FOCUS_NONE:
+					current_focus = temp_focus
+				else:
+					current_focus = temp_focus.get_node(temp_focus.focus_neighbour_top)
 				yield(get_tree(), "idle_frame")
-		elif event.is_action_pressed("ui_left") or (event is InputEventJoypadMotion and \
+				current_focus.grab_focus()
+		elif Input.is_action_just_pressed("ui_left") or (event is InputEventJoypadMotion and \
 								event.axis == JOY_AXIS_0 and event.axis_value == -1):
-				current_focus = current_focus.get_node(current_focus.focus_neighbour_left)
+				temp_focus = current_focus.get_node(current_focus.focus_neighbour_left)
+				if !temp_focus.focus_mode == Control.FOCUS_NONE:
+					current_focus = temp_focus
+				else:
+					current_focus = temp_focus.get_node(temp_focus.focus_neighbour_left)
 				yield(get_tree(), "idle_frame")
-		elif event.is_action_pressed("ui_right") or (event is InputEventJoypadMotion and \
+				current_focus.grab_focus()
+		elif Input.is_action_just_pressed("ui_right") or (event is InputEventJoypadMotion and \
 								event.axis == JOY_AXIS_0 and event.axis_value == 1):
-				current_focus = current_focus.get_node(current_focus.focus_neighbour_right)
+				temp_focus = current_focus.get_node(current_focus.focus_neighbour_right)
+				if !temp_focus.focus_mode == Control.FOCUS_NONE:
+					current_focus = temp_focus
+				else:
+					current_focus = temp_focus.get_node(temp_focus.focus_neighbour_right)
 				yield(get_tree(), "idle_frame")
-		elif event.is_action_pressed("ui_down") or (event is InputEventJoypadMotion and \
+				current_focus.grab_focus()
+		elif Input.is_action_just_pressed("ui_down") or (event is InputEventJoypadMotion and \
 								event.axis == JOY_AXIS_1 and event.axis_value == 1):
-				current_focus = current_focus.get_node(current_focus.focus_neighbour_bottom)
+				temp_focus = current_focus.get_node(current_focus.focus_neighbour_down)
+				if !temp_focus.focus_mode == Control.FOCUS_NONE:
+					current_focus = temp_focus
+				else:
+					current_focus = temp_focus.get_node(temp_focus.focus_neighbour_down)
 				yield(get_tree(), "idle_frame")
-		current_focus.grab_focus()
-		print(current_focus.name)
+				current_focus.grab_focus()
 
-	if event is InputEventJoypadButton:
-			if Input.is_action_just_pressed("ui_accept"):
-				if current_focus is Button:
-					print(current_focus.name)
-					current_focus.emit_signal("button_down")
-				elif current_focus is LineEdit:
-					print(current_focus.name)
-					$IPAdress/IPText/OnscreenKeyboard.visible = true
-					current_focus = $IPAdress/IPText/OnscreenKeyboard
-					yield(get_tree(), "idle_frame")
-					current_focus.grab_focus()
-			elif event.is_action_pressed("ui_cancel"):
-				$Buttons/BackButton.emit_signal("pressed")
+		if event is InputEventJoypadButton:
+				if Input.is_action_just_pressed("ui_accept"):
+					if current_focus is Button:
+						print("Accept Input - ", current_focus.name)
+						current_focus.emit_signal("pressed")
+					elif current_focus is LineEdit:
+						print("LineEdit - ", current_focus.name)
+						$IPAddress/IPText/OnscreenKeyboard.visible = true
+						current_focus = $IPAddress/IPText/OnscreenKeyboard
+						yield(get_tree(), "idle_frame")
+						current_focus.grab_focus()
+				elif Input.is_action_just_pressed("ui_cancel"):
+					$Buttons/BackButton.emit_signal("pressed")
 
 
-func _host():
-	print("Host")
-	$HostJoin/JoinBox.pressed = false
-	$HostJoin/HostBox.pressed = true
-	$IPAdress/IPText.text = Network.upnp.query_external_address()
-	$IPAdress/IPText.editable = false
+func _host(toggled : bool):
+	print("_host")
+	join_box.pressed = false
+	host_box.pressed = toggled
+	$IPAddress/IPText.text = Network.upnp.query_external_address()
+	$IPAddress/IPText.editable = toggled
 	is_host = true
-	enabled_lobby_button()
+	enabled_lobby_button(toggled)
 
 
-func _join():
-	print("Join")
-	$HostJoin/HostBox.pressed = false
-	$HostJoin/JoinBox.pressed = true
-	$IPAdress/IPText.editable = true
+func _join(toggled : bool):
+	print("_join")
+	host_box.pressed = false
+	join_box.pressed = toggled
+	$IPAddress/IPText.editable = true
 	is_host = false
-	enabled_lobby_button()
+	enabled_lobby_button(toggled)
 
 
 func _ip_set(new_text):
-	enabled_lobby_button()
+	enabled_lobby_button(host_box.pressed or join_box.pressed)
 
 
-func enabled_lobby_button() -> void:
-	if (host_box.pressed or join_box.pressed) and $IPAdress/IPText.text != "":
-		$Buttons/RaceButton.disabled = false
-		if is_host:
-			$Buttons/RaceButton.text = "Host"
-		else:
-			$Buttons/RaceButton.text = "Join"
+func enabled_lobby_button(toggled : bool) -> void:
+	print("enabled_lobby_button")
+	if toggled:
+		if $IPAddress/IPText.text != "":
+			$Buttons/RaceButton.disabled = false
+			$Buttons/RaceButton.focus_mode = Control.FOCUS_NONE
+
+	if !toggled:
+		$Buttons/RaceButton.disabled = true
+		$Buttons/RaceButton.focus_mode = Control.FOCUS_ALL
+		$Buttons/RaceButton.text = ""
+	elif is_host:
+		$Buttons/RaceButton.text = "Host"
+	else:
+		$Buttons/RaceButton.text = "Join"
 
 
 func _setup_online_lobby():
-	Network.set_IP_address($IPAdress/IPText.text)
+	Network.set_IP_address($IPAddress/IPText.text)
 	emit_signal("setup_online_lobby", is_host)
