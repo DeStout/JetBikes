ac3b1787b60d2ce066bd941c9c9f29a84c1993be
DeStout <destoutdev@gmail.com>
2022-12-04 19:39:45 -0800

    Normalize Lobby button colors.

--- Game/Singletons/Globals.gd
+++ Game/Singletons/Globals.gd
@@ -16,14 +16,14 @@ const DEFAULT_LAP_NUMBER : int = 3
 const MIN_LAP_NUMBER : int = 1
 const MAX_LAP_NUMBER : int = 99
 
-const DEFAULT_NPC_NUMBER : int = 0
+const DEFAULT_NPC_NUMBER : int = 11
 const MIN_NPC_NUMBER : int = 0
 const MAX_NPC_NUMBER : int = 11
 
-const DEFAULT_SFX_LEVEL : int = -40
+const DEFAULT_SFX_LEVEL : int = -30
 #const DEFAULT_SFX_LEVEL : int = -48
-#const DEFAULT_MUSIC_LEVEL : int = -40
-const DEFAULT_MUSIC_LEVEL : int = -48
+const DEFAULT_MUSIC_LEVEL : int = -30
+#const DEFAULT_MUSIC_LEVEL : int = -48
 const MIN_SFX_LEVEL : int = -48
 const MAX_SFX_LEVEL : int = -12
 const MIN_MUSIC_LEVEL : int = -48
